d07d7d096756384d02639cc765a4341ac842b27e
AlexHMercer <745164733@qq.com>
2024-06-14 12:54:10 +0800

    构建基本项目结构

---
+++ gulimail-order/src/test/java/com/alex/gulimail/order/GulimailOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.gulimail.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
