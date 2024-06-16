86b664ba1f7be1dc8eb2d04b3e1370012b86de20
AlexHMercer <745164733@qq.com>
2024-06-16 18:52:48 +0800

    完成基本的增删改查，并在product模块中进行测试

--- gulimail-product/src/main/java/com/alex/gulimail/product/GulimailProductApplication.java
+++ gulimail-product/src/main/java/com/alex/gulimail/product/GulimailProductApplication.java
@@ -1,8 +1,10 @@
 package com.alex.gulimail.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.alex.gulimail.product.dao")
 @SpringBootApplication
 public class GulimailProductApplication {
 
--- gulimail-product/src/test/java/com/alex/gulimail/product/GulimailProductApplicationTests.java
+++ gulimail-product/src/test/java/com/alex/gulimail/product/GulimailProductApplicationTests.java
@@ -1,13 +1,36 @@
 package com.alex.gulimail.product;
 
+import com.alex.gulimail.product.entity.BrandEntity;
+import com.alex.gulimail.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimailProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setId(4L);
+//        brandEntity.setName("HUAWEI");
+
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功");
+
+//        brandService.updateById(brandEntity);
+//        System.out.println("修改成功");
+        List<BrandEntity> idList = brandService.list(new QueryWrapper<BrandEntity>().eq("id", 1L));
+        idList.forEach((item)->{
+            System.out.println(item);
+        });
     }
 
 }
