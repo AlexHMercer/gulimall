30716f39c92811a651ae38b6679398d6aa0e8942
AlexHMercer <745164733@qq.com>
2024-06-22 20:08:08 +0800

    基础的配置中心设置，将优惠券服务的配置使用配置中心进行管理

--- gulimail-coupon/src/main/java/com/alex/gulimail/coupon/controller/CouponController.java
+++ gulimail-coupon/src/main/java/com/alex/gulimail/coupon/controller/CouponController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +28,24 @@ import com.alex.common.utils.R;
  * @email h745164733@foxmail.com
  * @date 2024-06-19 13:08:04
  */
+
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",name).put("age",age);
+    }
+
 
     // feign调用测试方法
     @RequestMapping("/member/list")
--- gulimail-member/src/main/java/com/alex/gulimail/member/feign/CouponFeignService.java
+++ gulimail-member/src/main/java/com/alex/gulimail/member/feign/CouponFeignService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
     Feign的远程调用：
     1. 引入相关依赖
     2. 在要调用远程服务的服务中建立一个接口
-    3. 将接口标注@FeignClient注解，注解中标记要目标服务的服务名
+    3. 将接口标注@FeignClient注解，注解中标记目标服务的服务名
     4. 将目标服务中的所需的方法签名贴过来，即方法注解+方法头
         注意：目标服务中的方法请求路径要写全（即若类上也有@RequestMapping注解，要把两个路径拼接）
     5. 开启远程调用功能，启动类上标注@EnableFeignClients(basePackages = "com.alex.gulimail.member.feign")
